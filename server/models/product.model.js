3066e1bf9a4d7281c9434b014f437a668186046e
nskhoa1502 <nskhoa1502@gmail.com>
2023-06-23 05:55:43 +0700

    add-product-model

---
+++ server/models/product.model.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose"); // Erase if already required
+const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
+
+// Declare the Schema of the Mongo model
+var productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    // Đồng hồ apple => slug: dong-ho-apple
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: mongoose.Types.ObjectId,
+      ref: "category",
+    },
+    quantity: {
+      type: Number,
+      default: 0,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    images: {
+      type: Array,
+    },
+    color: {
+      type: String,
+      enum: ["Black", "Red", "Brown", "Green", "Blue"],
+    },
+    ratings: [
+      {
+        star: { type: Number },
+        postedBy: {
+          type: mongoose.Types.ObjectId,
+          ref: "User",
+        },
+        comment: { type: String },
+      },
+    ],
+    totalRatings: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//Export the model
+module.exports = mongoose.model("Product", productSchema);
